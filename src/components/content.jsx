4fb6603aaf9f381c8f592ab34d106262b9aa27d0
Mike Pawlak <mpawlak719@gmail.com>
2022-01-31 15:48:20 -0600

    make static until I can containerize a scraper

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -8,7 +8,7 @@ class Content extends Component {
       profile: {},
       positions: [],
       education: [],
-      volunteerExp: []
+      volunteerExp: [],
     };
   }
 
@@ -18,7 +18,7 @@ class Content extends Component {
         profile: this.props.profile,
         positions: this.props.profile.positions.slice(0, 3),
         education: this.props.profile.educations,
-        volunteerExp: this.props.profile.volunteerExperience
+        volunteerExp: this.props.profile.volunteerExperience,
       });
     }
   }
@@ -32,7 +32,7 @@ class Content extends Component {
             <div className="col-12 col-md-8 jumbo-content">
               <h2 className="resume-header">Work Experience</h2>
               <ul className="positions">
-                {this.state.positions.map((el, i) => (
+                {/* {this.state.positions.map((el, i) => (
                   <li className="position" key={i}>
                     <div>
                       <h4>{el.title}</h4>
@@ -42,10 +42,55 @@ class Content extends Component {
                       <hr />
                     </div>
                   </li>
-                ))}
+                ))} */}
+                <li className="position">
+                  <div>
+                    <h4>Software Engineer II - Entegral</h4>
+                    <p>
+                      <em>Jan 2020 - present</em>
+                    </p>
+                    <hr />
+                  </div>
+                </li>
+                <li className="position">
+                  <div>
+                    <h4>Development Lead - Fleet Farm</h4>
+                    <p>
+                      <em>Jul 2019 - Jan 2020</em>
+                    </p>
+                    <hr />
+                  </div>
+                </li>
+                <li className="position">
+                  <div>
+                    <h4>Software Engineer - Fleet Farm</h4>
+                    <p>
+                      <em>Oct 2018 - Jul 2019</em>
+                    </p>
+                    <hr />
+                  </div>
+                </li>
+                <li className="position">
+                  <div>
+                    <h4>Senior Website Support Developer - Dealersocket</h4>
+                    <p>
+                      <em>Dec 2017 - Oct 2018</em>
+                    </p>
+                    <hr />
+                  </div>
+                </li>
+                <li className="position">
+                  <div>
+                    <h4>Website Support Developer - Dealersocket</h4>
+                    <p>
+                      <em>Aug 2017 - Dec 2017</em>
+                    </p>
+                    <hr />
+                  </div>
+                </li>
               </ul>
               <h2 className="resume-header">Education</h2>
-              {this.state.education.map((el, i) => (
+              {/* {this.state.education.map((el, i) => (
                 <div key={i} className="education-block">
                   <h4>{el.title}</h4>
                   <p>
@@ -55,9 +100,18 @@ class Content extends Component {
                     , {el.date2}
                   </p>
                 </div>
-              ))}
+              ))} */}
+              <div className="education-block">
+                <h4>University of Wisconsin Oshkosh</h4>
+                <p>
+                  <b>
+                    <em>Bachelor's of Science, Political Science</em>
+                  </b>
+                  , 2011- 2017
+                </p>
+              </div>
               <h2 className="resume-header">Volunteer Experience</h2>
-              {this.state.volunteerExp.map((el, i) => (
+              {/* {this.state.volunteerExp.map((el, i) => (
                 <div key={i} className="education-block">
                   <h4>{el.experience}</h4>
                   <p>
@@ -67,7 +121,16 @@ class Content extends Component {
                     , {el.date1}
                   </p>
                 </div>
-              ))}
+              ))} */}
+              <div className="education-block">
+                <h4>TEALS</h4>
+                <p>
+                  <b>
+                    <em>Computer Science Teaching Assistant</em>
+                  </b>
+                  , Jun 2019 - Dec 2019
+                </p>
+              </div>
             </div>
             <div className="col"></div>
           </div>
